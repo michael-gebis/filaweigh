9247e6e27b0d2a63d8a004071ce298bbcc807ec4
Michael Gebis <mike.gebis@gmail.com>
2024-02-19 15:19:12 -0800

    Initial commit; reads/prints load cell in a loop

---
+++ filaweigh.ino
@@ -0,0 +1,50 @@
+#include <Arduino.h>
+#include "HX711.h"
+
+// HX711 load cell amplifier
+const int LOADCELL_DOUT_PIN = 16;
+const int LOADCELL_SCK_PIN = 4;
+HX711 scale;
+
+void setup() {
+  Serial.begin(115200);
+  scale.begin(LOADCELL_DOUT_PIN, LOADCELL_SCK_PIN);
+}
+
+void l1() {
+  // put your main code here, to run repeatedly:
+  if (scale.is_ready()) {
+    scale.set_scale();    
+    Serial.println("Tare... remove any weights from the scale.");
+    delay(5000);
+    scale.tare();
+    Serial.println("Tare done...");
+    Serial.print("Place a known weight on the scale...");
+    delay(5000);
+    long reading = scale.get_units(10);
+    Serial.print("Result: ");
+    Serial.println(reading);
+  } 
+  else {
+    Serial.println("HX711 not found.");
+  }
+  delay(1000);
+}
+
+void l2() {
+  
+  if (scale.is_ready()) {
+    long reading = scale.read_average(5);
+    Serial.print("HX711 reading: ");
+    Serial.println(reading);
+  } else {
+    Serial.println("HX711 not found.");
+  }
+
+  delay(100);
+}
+
+void loop() {
+  l2();
+}
+
